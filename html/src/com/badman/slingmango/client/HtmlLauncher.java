e25ae8ed2d3c3bf9e938c0c9c626d0d896b77a0b
Roncon93 <roncon@Ronalds-MacBook-Pro.local>
2017-02-24 22:05:02 -0500

    Added libGDX project

---
+++ html/src/com/badman/slingmango/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.badman.slingmango.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badman.slingmango.SlingName;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new SlingName();
+        }
+}
---
+++ ios-moe/src/com/badman/slingmango/IOSMoeLauncher.java
@@ -0,0 +1,26 @@
+package com.badman.slingmango;
+
+import com.badlogic.gdx.backends.iosmoe.IOSApplication;
+import com.badlogic.gdx.backends.iosmoe.IOSApplicationConfiguration;
+import org.moe.natj.general.Pointer;
+import com.badman.slingmango.SlingName;
+
+import apple.uikit.c.UIKit;
+
+public class IOSMoeLauncher extends IOSApplication.Delegate {
+
+    protected IOSMoeLauncher(Pointer peer) {
+        super(peer);
+    }
+
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        config.useAccelerometer = false;
+        return new IOSApplication(new SlingName(), config);
+    }
+
+    public static void main(String[] argv) {
+        UIKit.UIApplicationMain(0, null, null, IOSMoeLauncher.class.getName());
+    }
+}
